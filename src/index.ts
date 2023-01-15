96462351b4c91676a1364cfdc2981824887e2760
Schubert Cardozo <cardozoschubert@gmail.com>
2023-01-15 10:47:43 +0000

    Auto reconnect (every 1 sec)

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,46 @@
 import * as net from 'net';
 
-let peerToServer: net.Socket;
+let server: net.Socket;
 let target: net.Socket;
 
 const connectToHub = () => {
-  peerToServer = net.connect(8002, 'localhost', () => {});
-  peerToServer.on('data', (data) => {
-    const cmd = data.toString().substring(0, 3);
-    if (cmd === 'GO!') {
-      const targetUrl = data.toString().split(' ')[1];
-      target = net.createConnection(443, targetUrl, function () {});
-      peerToServer.pipe(target);
-      target.pipe(peerToServer);
-    }
-  });
+  try {
+    if (server && server.readyState === 'open') return;
+    console.log('reconnect attempt');
+    server = net.createConnection({ allowHalfOpen: false, host: 'localhost', port: 8002 }, () => {});
+    server.write('IDENTIF');
+    server.on('data', (data) => {
+      const cmd = data.toString().substring(0, 3);
+      if (cmd === 'GO!') {
+        const targetUrl = data.toString().split(' ')[1];
+        target = net.createConnection({ allowHalfOpen: false, port: 443, host: targetUrl }, function () {});
+        server.pipe(target);
+        target.pipe(server);
+      } else if (cmd === 'GO2') {
+        const targetUrl = data.toString().split(' ')[1];
 
-  peerToServer.on('close', () => {
-    console.log('recycling peer');
-    connectToHub();
-  });
+        target = net.createConnection({ allowHalfOpen: false, port: 80, host: targetUrl }, function () {});
+        target.pipe(server);
+        server.write('OK');
+        server.pipe(target);
+      } else {
+        console.log('data', data.toString());
+      }
+    });
+
+    server.on('close', () => {
+      console.log('auto reconnect');
+      connectToHub();
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-connectToHub();
+// const waitForHub = () => {
+setInterval(() => {
+  connectToHub();
+}, 1000);
+// };
+
+// connectToHub();
